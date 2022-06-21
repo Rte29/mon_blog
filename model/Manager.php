ee468106fb7627c03352179161a409c7d851db8c
R1 <er.gouez@gmail.com>
2022-06-21 11:36:36 +0200

    indentation model manager

--- model/Manager.php
+++ model/Manager.php
@@ -7,13 +7,13 @@ class Manager
     protected function dbConnect()
     {
         try
-        {
-        $db = new \PDO('mysql:host=localhost;dbname=mon_blog;charset=utf8', 'root', '');
-        return $db;
-        }
+            {
+                $db = new \PDO('mysql:host=localhost;dbname=mon_blog;charset=utf8', 'root', '');
+                return $db;
+            }
         catch (Exception $e)
-        {
-            die('Erreur : ' . $e->getMessage());
-        }
+            {
+                die('Erreur : ' . $e->getMessage());
+            }
     }
 }
